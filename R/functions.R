a7835d021859722a5540d652ab894a5ba1a5dd39
Loukas Theodosiou <theodosiou@evolbio.mpg.de>
2022-12-26 23:11:20 +0100

    :bee: adding the theme package

---
+++ R/functions.R
@@ -0,0 +1,67 @@
+#' telca: A package to make plotting with ggplot2 a bit easier
+#'
+#' @name telca
+#' @import  ggplot2
+#' @import  ggtext
+#'
+#' @param base_size base font size
+#' @param base_line_size,base_rect_size,base_family check#'
+#'
+#' @details The Roboto Condensed and Roboto Bold fonts are both Google fonts;
+#' they can be found at \url{https://fonts.google.com/specimen/Roboto+Condensed}
+#' and \url{https://fonts.google.com/specimen/Roboto}. These fonts must be
+#' installed locally on your computer for this theme to work.
+#'
+#' @examples
+#' \dontrun{
+#' library(ggplot2)
+#'
+#' ggplot(mtcars, aes(wt, mpg)) +
+#'     geom_point() +
+#'     labs(title = "A Lovely Plot",
+#'          subtitle = "What can the subtitle tell us?") +
+#'     theme_pres()
+#'}
+#'
+#' @export
+
+theme_pres <- function(
+    base_size =12,
+    base_line_size = base_size/24,
+    base_rect_size = base_size/24,
+    base_family = "Roboto"
+){
+  ggplot2::theme_bw(
+    base_size = base_size,
+    base_family = base_family,
+    base_line_size = base_line_size,
+    base_rect_size = base_rect_size
+  ) %+replace%
+    ggplot2::theme(
+      axis.text.x = element_text(size = 15, color = "grey30", margin=margin(t=5,b=10)),
+      axis.text.y = element_text(size = 15, color = "grey30", margin=margin(t=0, r = 5, b = 0, l = 10)),
+      axis.ticks = element_line(color = "grey91", size = .5),
+      axis.ticks.length.x = unit(.5, "lines"),
+      axis.ticks.length.y = unit(.7, "lines"),
+      axis.title=element_text(size=18),
+      legend.text=element_text(size=15),
+      legend.title=element_text(size=18, hjust = 0.5),
+      panel.grid.major = element_line(color=NA),
+      plot.margin = margin(20, 40, 20, 40),
+      plot.background = element_rect(fill = "grey98", color = "grey98"),
+      panel.background = element_rect(fill = "grey98", color = "grey98"),
+      legend.key= element_rect(fill = "grey98", colour = "grey98"),
+      legend.background= element_rect(fill = "grey98", colour = "grey98"),
+      plot.title = element_text(color = "grey10", size = 20, face = "bold",
+                                margin = margin(t = 25,b=10), hjust=0.5),
+      plot.subtitle = element_markdown(color = "grey30", size = 12,
+                                       lineheight = 1.35, hjust=0.5),
+      #margin = margin(t = 15, b = 40)), # i have no idea what is it doing
+      plot.caption = element_text(color = "grey30", size = 10,
+                                  lineheight = 1.2, hjust = 0,
+                                  margin = margin(t = 80)),
+      #legend.position = "none"
+      complete=TRUE
+    )
+}
+
